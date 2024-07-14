ac3345308b3c987831d6512869a6c00a632a8f63
Mei Joure Racpan <44115821+Meiji01@users.noreply.github.com>
2024-07-15 00:50:51 +0800

    first commit

---
+++ AgentContainer/Service1.Designer.vb
@@ -0,0 +1,60 @@
+﻿Imports System.ServiceProcess
+Imports System.Threading
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Service1
+    Inherits System.ServiceProcess.ServiceBase
+
+    'UserService overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    ' The main entry point for the process
+    <MTAThread()>
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Shared Sub Main()
+        Dim ServicesToRun() As System.ServiceProcess.ServiceBase
+
+        ' More than one NT Service may run within the same process. To add
+        ' another service to this process, change the following line to
+        ' create a second service object. For example,
+        '
+        '   ServicesToRun = New System.ServiceProcess.ServiceBase () {New Service1, New MySecondUserService}
+        '
+        ServicesToRun = New System.ServiceProcess.ServiceBase() {New Service1}
+
+        System.ServiceProcess.ServiceBase.Run(ServicesToRun)
+
+        'Console.WriteLine("Hello world")
+        'Call executeJenkinsAgent("test")
+
+        'Dim threadmain As Thread
+        'threadmain = New Thread(New ThreadStart(AddressOf executeJenkinsAgent))
+        'threadmain.Start()
+
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    ' NOTE: The following procedure is required by the Component Designer
+    ' It can be modified using the Component Designer.  
+    ' Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container()
+        Me.ServiceName = "JenkAgentContainer"
+    End Sub
+
+
+
+
+End Class
---
+++ AgentContainer/Service1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Threading
+Imports System.Timers
+
+Public Class Service1
+
+    Dim threadmain As Thread
+    Dim oProcess As Process
+    Dim agentconnected As Boolean
+    Dim servicearg As String
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+
+        'initialize arguments
+        'servicearg = args(1)
+        servicearg = "C:\Jenkins\agent.jar -url http://192.168.100.33:8080/jenkins/ -secret 5c109df679c57e2891f0602d9dd6f51177c81141dbbc06b7c159e19365bdd6ca -name Win10Virtual -workDir ""C:\Jenkins"""
+
+        'processfactory.createCmdSession()
+        ' Add code here to start your service. This method should set things
+        ' in motion so your service can do its work.
+        EventLog.WriteEntry("Starting Jenkins Agent at arguments: -jar " & servicearg)
+        agentconnected = False
+
+
+
+        'Dim threadmain As Thread
+        threadmain = New Thread(New ThreadStart(AddressOf executeJenkinsAgent))
+        threadmain.Start()
+        'EventLog.WriteEntry("Jenkins Session ID:" & processfactory.getId)
+        'executeJenkinsAgent()
+
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        EventLog.WriteEntry("Stopping Jenkins Agent:" & oProcess.Id)
+        oProcess.Kill()
+        'oProcess.StandardOutput.Close()
+        'oProcess.Close()
+        End
+        'oProcess.StandardInput.WriteLine("exit")
+
+        ' Add code here to perform any tear-down necessary to stop your service.
+    End Sub
+
+
+
+
+    Sub executeJenkinsAgent()
+
+        Dim oSession As New createCmd()
+        oSession.createJavaSession(servicearg)
+        oProcess = oSession.getProcess
+
+        'processfactory.createCmdSession()
+        'Dim oProcess As Process
+        'oProcess = processfactory.getProcess
+        EventLog.WriteEntry("Jenkins Session ID:" & oProcess.Id)
+        'oProcess.StandardInput.WriteLine("C:\Jenkins\runagent.bat")
+        'oProcess.StandardInput.WriteLine("Y")
+        'processfactory.getId = oProcess.SessionId
+
+        'While (oProcess.StandardOutput.EndOfStream = False)
+        'Dim outputstream As String
+        'outputstream = oProcess.StandardOutput.ReadLine
+        'Console.WriteLine(outputstream)
+        'Debug.WriteLine(outputstream)
+        'End While
+
+        While (oProcess.StandardError.EndOfStream = False)
+            Dim outputerror As String
+            outputerror = oProcess.StandardError.ReadLine
+            Debug.WriteLine(outputerror)
+        End While
+
+        'oProcess.StandardInput.Close()
+    End Sub
+
+End Class
